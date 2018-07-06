3e93f6c049d0e38f62be32ed6b109193c72733f0
itmePatatus <nicolaurve@gmail.com>
2018-07-06 18:41:17 +0200

    ejercicio uno

--- script.py
+++ script.py
@@ -1,2 +1,13 @@
-a = 5
-print (a)
+a = 10
+while a >= 0:
+    if a == 0:
+        print(a)
+
+    elif a%2 == 0:
+        print(a, 'es par')
+
+    else:
+        print(a, 'no es par')
+    a = a - 1
+
+# n = int(input(''))
